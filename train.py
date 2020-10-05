ed1655144f578287abba53fa8bb0f792de8b6611
tikzoxs <tikzoxs@gmail.com>
2020-10-05 20:00:24 +1300

    new improved and normailzed generator for 160 x 120

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ history = model.fit_generator(
 	steps_per_epoch=10000, epochs=75,
 	verbose=1, callbacks=[checkpointer,reduce_lr], 
 	validation_data=generate_dataset.generateImagesValidation(), validation_steps=500, validation_freq=1, class_weight=None, 
-	max_queue_size=10, workers=1, use_multiprocessing=False, shuffle=True, initial_epoch=44)
+	max_queue_size=10, workers=1, use_multiprocessing=False, shuffle=True, initial_epoch=0)
 
 print('\nhistory dict:', history.history)
 
